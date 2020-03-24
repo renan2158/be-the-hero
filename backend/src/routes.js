817b9be63c15f0e76eab2c706b81d4730efaebc6
renan2158 <renan2158@gmail.com>
2020-03-24 18:32:09 -0300

    Application back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = express.Router();
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentsController');
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+module.exports = routes;
